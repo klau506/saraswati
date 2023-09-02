429a27bc36a30ae6dfc59a3ca98196b41da8e014
klau506 <klaudia.krb@gmail.com>
2023-09-02 18:09:15 +0200

    Dynamizer: clean code

--- google_script.ts
+++ google_script.ts
@@ -1,77 +1,95 @@
-function insertDataInSheet2(mail_vec, ensenyar_vec, aprendre_vec) {
-  var spreadsheet = SpreadsheetApp.openById("1nRQibzFbMquGppsPTQTK0P1rmbx6WUiXhzGhxOZLE50"); // Replace with your spreadsheet ID
-  var sheet = spreadsheet.getSheetByName("Groups"); // Replace with your sheet name
+// insertDataInGroupsSheet
+// @return: insert given data in the groups' sheet computing the group id
+// @mail_vec: vector of mails (position 0 = person1, position 1 = person 2)
+// @teach_vec: vector of languages to teach (position 0 = person1, position 1 = person 2)
+// @learn_vec: vector of languages to learn (position 0 = person1, position 1 = person 2)
+function insertDataInGroupsSheet(mail_vec, teach_vec, learn_vec) {
+  var spreadsheet = SpreadsheetApp.openById("1nRQibzFbMquGppsPTQTK0P1rmbx6WUiXhzGhxOZLE50");
+  var sheet = spreadsheet.getSheetByName("Groups");
 
-  // Get the last row with content in the sheet
+  // last row of groups' sheet
   var lastRow = sheet.getLastRow();
+
+  // compute id of the new group
   var id = sheet.getRange("A"+lastRow).getValue() + 1
 
+  // desing dataset to introduce
   var data = [
-    [id, mail_vec[0], ensenyar_vec[0], aprendre_vec[0]],
-    [id, mail_vec[1], ensenyar_vec[1], aprendre_vec[1]],
+    [id, mail_vec[0], teach_vec[0], learn_vec[0]],
+    [id, mail_vec[1], teach_vec[1], learn_vec[1]],
   ];
 
-  // Insert the data starting from the next row
+  // insert data in the groups' sheet
   sheet.getRange(lastRow + 1, 1, data.length, data[0].length).setValues(data);
 }
 
+
+// cleanResponses
+// @return: copy last entry of the answers' sheet to the cleaned_data sheet. In case
+// a person introduces more than one language to learn or to teach, create multiple
+// columns with only one of the possible combinations
 function cleanResponses () {
   var spreadsheet = SpreadsheetApp.openById("1nRQibzFbMquGppsPTQTK0P1rmbx6WUiXhzGhxOZLE50");
-  var sheet = spreadsheet.getSheetByName("Respostes al formulari 1");
-  var cd_sheet = spreadsheet.getSheetByName("Cleaned_data");
+  var sheet = spreadsheet.getSheetByName("Respostes al formulari 1");   // answers sheet
+  var cd_sheet = spreadsheet.getSheetByName("Cleaned_data");    // cleaned_data sheet
 
+  // last row of answers' sheet
   var lastRow = sheet.getLastRow()
 
   var teach_original = sheet.getRange("E"+lastRow).getValue()
   var learn_original = sheet.getRange("F"+lastRow).getValue()
-
   var teach_vec = teach_original.split(",");
   var learn_vec = learn_original.split(",");
 
-  // Get the data from the row to duplicate
+  // get the data from the current row
   var tmp_row = sheet.getRange(lastRow, 1, 1, sheet.getLastColumn()).getValues()[0];
+  // create all possible combinations of teach-learn language
   for (var i = 0; i < teach_vec.length; i++) {
     for (var j = 0; j < learn_vec.length; j++) {
       // modify row with the current teaching and learning languages
       tmp_row[4] = teach_vec[i];
       tmp_row[5] = learn_vec[j];
 
+      // insert cleaned row to the cleaned_data sheet
       var cd_lastRow = cd_sheet.getLastRow()
       cd_sheet.getRange(cd_lastRow + 1, 1, 1, tmp_row.length).setValues([tmp_row]);
     }
   }
 }
 
+// readDataFromSpreadsheet
+// @return: read data from answers' sheet, check for matches and create the groups
 function readDataFromSpreadsheet() {
-  var spreadsheet = SpreadsheetApp.openById("1nRQibzFbMquGppsPTQTK0P1rmbx6WUiXhzGhxOZLE50"); // Replace with your spreadsheet ID
-  var sheet = spreadsheet.getSheetByName("Respostes al formulari 1"); // Replace with your sheet name
+  var spreadsheet = SpreadsheetApp.openById("1nRQibzFbMquGppsPTQTK0P1rmbx6WUiXhzGhxOZLE50");
+  var sheet = spreadsheet.getSheetByName("Respostes al formulari 1");
 
-  var mails = sheet.getRange("B2:B4").getValues(); // Read data from columns A and B
-  var ensenyar = sheet.getRange("E2:E4").getValues(); // Read data from columns A and B
-  var aprendre = sheet.getRange("F2:F4").getValues(); // Read data from columns A and B
+  // TODO: make it automatic to read from the second row to the last one with text
+  var mails = sheet.getRange("B2:B4").getValues();
+  var teach = sheet.getRange("E2:E4").getValues();
+  var learn = sheet.getRange("F2:F4").getValues();
 
-  for (var e = 0; e < ensenyar.length; e++) {
-    for (var a = e + 1; a < aprendre.length; a++) {
-      var ensenyar1Value = ensenyar[e][0];  // idioma q ensenya la persona 1
-      var aprendre2Value = aprendre[a][0];  // idioma q aprèn la persona 2
+  for (var i = 0; i < teach.length; i++) {
+    for (var j = i + 1; j < learn.length; j++) {
+      var teach1_value = teach[i][0];  // language that person1 teaches
+      var learn2_value = learn[j][0];  // language that person2 learns
 
-      // si hi ha match
-      if (ensenyar1Value == aprendre2Value) {
-        // comprovem que la persona 1 vulgui aprendre el mateix idioma que el que ensenya la persona 2
-        aprendre1Value = aprendre[e][0];
-        ensenyar2Value = ensenyar[a][0];
+      // if there's match
+      if (teach1_value == learn2_value) {
+        // check that the language that person1 learns is the same that person2 teaches
+        learn1_value = learn[i][0];
+        teach2_value = teach[j][0];
 
-        if (aprendre1Value == ensenyar2Value) {
-          Logger.log("Match!: " + mails[e][0] + " ensenya " + ensenyar1Value + " i " + mails[a][0] + " ensenya " + ensenyar2Value);
-          insertDataInSheet2([mails[a][0],mails[e][0]], [ensenyar[a][0],ensenyar[e][0]], [aprendre[a][0],aprendre[e][0]])
+        if (learn1_value == teach2_value) {
+          Logger.log("Match!: " + mails[i][0] + " ensenya " + teach1_value + " i " + mails[j][0] + " ensenya " + teach2_value);
+          insertDataInSheet2([mails[j][0],mails[i][0]], [teach[j][0],teach[i][0]], [learn[j][0],learn[i][0]])
         }
       }
     }
   }
 }
 
-
+// TODO
 function main () {
   // send last row to "claned_data" sheat
-  // check groups again
+  // check groups again with readDataFromSpreadsheet function but using the cleaned data sheet instead of the answers one
 }
